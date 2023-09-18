dba0c7df4ede56e814454462348411f51aa2781f
Emmanuel Velez <emmanuel.velezme@gmail.com>
2023-09-17 22:08:59 -0500

    Login

---
+++ app-tasks/pages/home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <center>
+      <h1>Bienvenidos a la app de tareas</h1>
+      <p>Esta es una aplicación que permite gestionar las tareas de los estudiantes</p>
+
+      <a href="/tareas">Click para iniciar (sin nuxtLink)</a>
+<br>
+<br>
+<NuxtLink to="/tareas">
+        Ir a app de tareas (con nuxtLink)</NuxtLink>
+<br>
+<br>
+
+      <NuxtLink to="/">
+        Ir a Inicio de Sesión</NuxtLink>
+    </center>
+
+  </div>
+
+
+</template>
+
+
+
+<script>
+definePageMeta({
+  layout: "blank",
+});
+</script>
+
